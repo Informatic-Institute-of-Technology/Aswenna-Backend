b763e921ae11a0272ab80b10db106f079b3e6486
seshan <seshankavisanka@gmail.com>
2025-11-13 01:32:48 +0530

    use configuration for dynamic port and global prefix

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const configService = app.get(ConfigService);
+  const globalPrefix = configService.get<string>('app.apiPrefix') || 'api';
+  const port = configService.get<number>('app.port') || 3000;
+
+  app.setGlobalPrefix(globalPrefix);
+  await app.listen(port);
+
+  Logger.debug(
+    `Server is running on: http://localhost:${port}/${globalPrefix}`,
+    'Aswenna',
+  );
 }
 bootstrap();
