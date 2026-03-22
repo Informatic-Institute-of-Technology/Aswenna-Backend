a64085e1f14b4160f8179038ecc685c810b51378
githmi-inivos <githmi.pandigama@inivosglobal.com>
2026-03-23 02:31:15 +0530

    feat: files updated -ASWN-143

--- src/modules/land-owner/ads/dtos/land-owner-ad.update.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateLandOwnerAdDto } from './land-owner-ad.create.dto';
-
-export class UpdateLandOwnerAdDto extends PartialType(CreateLandOwnerAdDto) {}
