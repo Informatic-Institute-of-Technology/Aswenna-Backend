b5e19854f9a57984f912a08d5a1159263acd2f4c
Geethma <geethmathuduwawattage@gmail.com>
2026-03-22 21:31:36 +0530

    feat(request querry dto): ASWN-141

--- src/modules/request/dtos/request.query.dto.ts
+++ src/modules/request/dtos/request.query.dto.ts
@@ -1,10 +1,38 @@
-import { IsMongoId, IsNotEmpty } from 'class-validator';
+import {
+  IsIn,
+  IsMongoId,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+} from 'class-validator';
 import { PaginationDto } from 'src/common/dtos/pagination.dto';
+import { RequestTargetType } from '../schemas/request.schema';
 
-export class RequestQueryDto extends PaginationDto {}
+export class RequestQueryDto extends PaginationDto {
+  @IsOptional()
+  @IsString()
+  @IsIn(Object.values(RequestTargetType))
+  targetType?: RequestTargetType;
+
+  @IsOptional()
+  @IsMongoId()
+  target?: string;
+
+  @IsOptional()
+  @IsMongoId()
+  recipient?: string;
+
+  @IsOptional()
+  @IsMongoId()
+  receiver?: string;
+
+  @IsOptional()
+  @IsString()
+  status?: string;
+}
 
 export class RequestParamsDto {
   @IsMongoId()
   @IsNotEmpty()
-  readonly requestId: string;
+  readonly id: string;
 }
