8323e055dcce336284dad6b906ae11a13061689e
githmi-inivos <githmi.pandigama@inivosglobal.com>
2026-03-23 02:47:27 +0530

    feat: files updated -ASWN-143

--- src/modules/permission/dtos/permission.update.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreatePermissionDto } from './permission.create.dto';
-
-export class UpdatePermissionDto extends PartialType(CreatePermissionDto) {}
